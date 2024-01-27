8fe9f5fb113ee7d8752c33e0f1fb14961fc6a794
noanswerss <ciprianvlad888@gmail.com>
2024-01-27 23:47:57 +0000

    ready to present

--- to-do-list/resources/views/edit.blade.php
+++ to-do-list/resources/views/edit.blade.php
@@ -1,4 +1,3 @@
-<!-- resources/views/edit.blade.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,13 +8,17 @@
     <title>Edit Task</title>
 </head>
 <body class="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 min-h-screen flex items-center justify-center">
-    <h2>Edit Task</h2>
-    <form action="{{ route('update', $todolist->id) }}" method="POST">
-        @csrf
-        @method('patch')
-        <label for="content">Task:</label>
-        <input type="text" name="content" value="{{ $todolist->content }}">
-        <button type="submit">Update Task</button>
-    </form>
+    <div class="bg-white p-8 rounded-lg shadow-md w-2/4">
+        <h2 class="text-center text-2xl text-gray-800 mb-4">Edit Task</h2>
+        <form action="{{ route('update', $todolist->id) }}" method="POST">
+            @csrf
+            @method('patch')
+            <div class="mb-4">
+                <label for="content" class="text-gray-600 block">Task:</label>
+                <input type="text" name="content" maxlength="70" value="{{ $todolist->content }}" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500">
+            </div>
+            <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300">Update Task</button>
+        </form>
+    </div>
 </body>
 </html>
--- to-do-list/resources/views/home.blade.php
+++ to-do-list/resources/views/home.blade.php
@@ -12,69 +12,68 @@
     <title>Todo List</title>
 </head>
 
-<body class="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 min-h-screen flex items-center justify-center">
-    <div class="flex items-center w-2/4">
+<body class="bg-gradient-to-r from-pink-500 via-purple-350 to-indigo-500 min-h-screen flex items-center justify-center">
+    <div class="w-full sm:w-4/5 md:w-3/5 lg:w-2/5 xl:w-2/5">
         <div class="w-full">
-            <div class="w-50">
-                <h1 class="text-center text-white text-6xl">To-Do List</h1>
-                <form action="{{ route('store') }}" method="POST" autocomplete="off" class="p-2">
-                    @csrf
-                    <div class="flex flex-row justify-between p-2">
-                        <input type="text" name="content" placeholder="Add your Task" class="border p-2 w-3/4" />
-                        <span>
-                            <button type="submit" class="bg-blue-500 text-white p-2 w-60 h-full hover:bg-pink-500 transition duration-300 ease-in-out">
-                                <i class="fa-solid fa-plus"></i>
-                            </button>
-                        </span>
-                    </div>
-                </form>
+            <h1 class="text-center text-white text-6xl">To-Do List</h1>
+            <form action="{{ route('store') }}" method="POST" autocomplete="off" class="p-2">
+                @csrf
+                <div class="flex flex-row justify-between p-2">
+                    <input type="text" name="content" maxlength="70" placeholder="Add your Task" class="p-2 w-3/4 rounded-tl-md rounded-bl-md" />
+                    <span>
+                        <button type="submit" class="bg-blue-500 text-white p-2 w-60 h-full hover:bg-pink-500 transition duration-300 ease-in-out rounded-tr-md rounded-br-md">
+                            <i class="fa-solid fa-plus"></i>
+                        </button>
+                    </span>
+                </div>
+            </form>
 
-                <!-- If Task list code starts -->
-                @if(count($todolists))
-                <ul class="flex flex-col">
-                    @foreach($todolists as $todolist)
-                    @unless($todolist->completed)
-                    <li class="flex flex-row justify-between p-2 bg-gradient-to-r from-blue-200 via-purple-150 to-blue-100 mx-4 mb-2 rounded-md">
-                        <span class="mr-auto">{{ $todolist->content }}</span>
-                        <!-- Edit Form -->
-                        <form action="{{ route('edit', $todolist->id) }}" method="GET">
-                            <button type="submit" class="bg-blue-500 text-white p-2">
-                                <i class="fa-solid fa-pencil hover:text-black transition-text duration-500"></i>
-                            </button>
-                        </form>
-
-                        <!-- Delete Form -->
-                        <form action="{{ route('destroy', $todolist->id) }}" method="POST">
-                            @csrf
-                            @method('delete')
-                            <button type="submit" class="bg-red-500 text-white p-2 h-full">
-                                <i class="fa-solid fa-trash hover:text-black transition-text duration-500"></i>
-                            </button>
-                        </form>
-
-                        <form action="{{ route('complete', $todolist->id) }}" method="POST">
-                            @csrf
-                            <button type="submit" class="h-full">
-                                <p class="text-blue-500 ml-7 mr-2 font-bold hover:text-green-500 transition duration-500 ease-out">Mark as complete</p>
-                            </button>
-                        </form>
-                    </li>
-                    @endunless
-                    @endforeach
-                </ul>
-                @else
-                <p class="text-white p-2">No Tasks!</p>
-                @endif
-                <!-- If Task list code Ends -->
-            </div>
+            <!-- If Task list code starts -->
             @if(count($todolists))
-            <div>
-                <h2 class="text-white p-2 text-center">You have {{ $todolists->where('completed', false)->count() }} pending tasks</h2>
-            </div>
+            <ul class="flex flex-col">
+                @foreach($todolists as $todolist)
+                @unless($todolist->completed)
+                <li class="flex flex-row items-center justify-between p-2 bg-gradient-to-r from-blue-200 via-purple-150 to-blue-100 mx-4 mb-2 rounded-md">
+                    <span class="mr-auto">{{ $todolist->content }}</span>
+                    <!-- Delete Form -->
+
+
+                    <form action="{{ route('complete', $todolist->id) }}" method="POST">
+                        @csrf
+                        <button type="submit" class="h-full">
+                            <p class="text-blue-500 ml-7 mr-2 font-bold hover:text-green-500 transition duration-500 ease-out">Mark as complete</p>
+                        </button>
+                    </form>
+                    <!-- Edit Form -->
+                    <form action="{{ route('edit', $todolist->id) }}" method="GET">
+                        <button type="submit" class="bg-blue-500 text-white p-2">
+                            <i class="fa-solid fa-pencil hover:text-black transition-text duration-500"></i>
+                        </button>
+                    </form>
+                    <form action="{{ route('destroy', $todolist->id) }}" method="POST">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="bg-red-500 text-white p-2">
+                            <i class="fa-solid fa-trash hover:text-black transition-text duration-500"></i>
+                        </button>
+                    </form>
+                </li>
+                @endunless
+                @endforeach
+            </ul>
             @else
-            <!-- Display nothing -->
+            <p class="text-white p-2">No Tasks!</p>
             @endif
+            <!-- If Task list code Ends -->
         </div>
+
+        @if(count($todolists))
+        <div>
+            <h2 class="text-white p-2 text-center font-bold">You have {{ $todolists->where('completed', false)->count() }} pending tasks</h2>
+        </div>
+        @else
+        <!-- Display nothing -->
+        @endif
     </div>
 </body>
 
